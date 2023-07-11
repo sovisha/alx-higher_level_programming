c47b879dd678bc4eab739f29315d178c8589f9de
sovisha <soviget@gmail.com>
2023-07-11 12:31:02 -0700

    working

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,19 +1,15 @@
 #!/usr/bin/python3
-""" Module that contains a function that writes to a text file
 """
+3-to_json_string module
+"""
+import json
 
 
-def write_file(filename="", text=""):
-    """ Function that writes to a text file
-
-    Args:
-        filename: filename
-        text: text to write
-
-    Raises
-        Exception: when the file can be opened
-
+def to_json_string(my_obj):
     """
-
-    with open(filename, 'w', encoding="utf-8") as f:
-        return f.write(text)
+    to_json_string - returns the JSON representation of an object (string):
+    Args:
+        my_obj: string to represent
+    Return: json representation
+    """
+    return json.dumps(my_obj)
